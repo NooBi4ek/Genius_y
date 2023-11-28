727cc98f70134ea234ebd5f967ae56fac82828fe
Maksim <gajdukmaksim710@gmail.com>
2023-11-28 23:18:25 +0300

    First day to project 28.11.23

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { FC } from 'react';
+import MainLayout from './layout/MainLayout';
+import { Box } from '@mui/material';
+import './App.css';
+
+const App: FC = () => {
+  return (
+    <Box sx={{ width: '100%', height: '100vh', backgroundColor: '#050810' }}>
+      <MainLayout />
+    </Box>
+  );
+};
+
+export default App;
---
+++ src/components/Button.tsx
@@ -0,0 +1,31 @@
+import { FC } from 'react';
+import { Button as MuiButton, styled } from '@mui/material';
+
+interface Props {
+  variant: 'outlined' | 'contained' | 'text';
+  size: 'small' | 'medium' | 'large';
+  text: string;
+}
+
+const variantStyles = () => ({
+  outlined: {
+    borderRadius: '5px',
+  },
+  contained: { borderRadius: '30px' },
+  text: {},
+});
+
+const Button: FC<Props> = ({ variant, size, text }) => {
+  return (
+    <StyledButton variant={variant} size={size}>
+      {text}
+    </StyledButton>
+  );
+};
+
+export default Button;
+
+const StyledButton = styled(MuiButton)(({ variant, size }) => ({
+  ...variantStyles()[variant || 'outlined'],
+  background: 'linear-gradient(90deg, #ff7e4a 35%, rgba(255,74,139,1) 100%)',
+}));
